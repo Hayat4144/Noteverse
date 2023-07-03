2a9aa29ad85e5defa58696dd7520c0c1aacda670
Hayat ilyas <ihayat855@gmail.com>
2023-07-03 13:19:31 +0530

    addding cors configuration and error middlware.

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,37 @@ import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
-dotenv.config(); 
+import logger from './utils/logger';
+import ErrorMiddleware from './middlewares/ErroMiddleware';
+import { CustomError } from './utils/CustomError';
 
 const app = express();
 app.use(cookieParser());
-app.use(cors());
+dotenv.config(); 
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? [
+            "https://frontend-hayat4144.vercel.app",
+            "https://dashboard-hayat4144.vercel.app",
+          ]
+        : ["http://localhost:3000"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
+
+
+
 const port = process.env.PORT || 8000;
 
 app.get('/', (req: Request, res: Response) => {
   return res.send('hello hayat ilyas');
 });
 
+
+app.use(ErrorMiddleware)
 app.listen(port, () => {
-  console.log(`Server is running at port ${port}`);
+  logger.info(`Server is running at port ${port}`)
 });
