432225b8879616a62982abca581b8103f3c90d7e
Hayat ilyas <ihayat855@gmail.com>
2023-07-09 17:10:46 +0530

    building auth form

---
+++ client/src/app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Image from 'next/image';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
+
+interface AuthlayoutProps {
+  children: React.ReactNode;
+}
+
+export default function Authlayout({ children }: AuthlayoutProps) {
+  return (
+    <main className="grid min-h-screen grid-cols-1 overflow-hidden md:grid-cols-3 lg:grid-cols-2">
+      <AspectRatio ratio={16 / 9}>
+        <Image
+          src={'/image/auth-layout.jpg'}
+          alt="note image"
+          priority
+          fill
+          className="absolute inset-0 object-cover"
+        />
+      </AspectRatio>
+      <section className="container absolute top-1/2 col-span-1 flex  -translate-y-1/2 items-center md:justify-center md:static md:top-0 md:col-span-2 md:flex md:translate-y-0 lg:col-span-1">
+        {children}
+      </section>
+    </main>
+  );
+}
---
+++ client/src/app/(auth)/signin/page.tsx
@@ -0,0 +1,60 @@
+import OAuthSignin from '@/components/auth/OAuthSignin';
+import Signinform from '@/components/forms/Signinform';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import Link from 'next/link';
+import React from 'react';
+
+export default function Signin() {
+  return (
+    <Card className="max-w-lg w-full">
+      <CardHeader className="space-y-1">
+        <CardTitle>Signin</CardTitle>
+        <CardDescription className="">
+          choose your preffered signin method
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+        <OAuthSignin />
+        <div className="single-signin relative">
+          <div className="absolute inset-0 flex items-center">
+            <span className="w-full border-t" />
+          </div>
+          <div className="relative flex justify-center text-xs uppercase">
+            <span className="bg-background px-2 text-muted-foreground">
+              Or continue with
+            </span>
+          </div>
+        </div>
+        <Signinform />
+      </CardContent>
+      <CardFooter className="flex flex-wrap items-center justify-between gap-2">
+        <div className="text-sm text-muted-foreground">
+          <span className="mr-1 hidden sm:inline-block">
+            Don&apos;t have an account?
+          </span>
+          <Link
+            aria-label="Sign up"
+            href="/signup"
+            className="text-primary underline-offset-4 transition-colors hover:underline"
+          >
+            Sign up
+          </Link>
+        </div>
+        <Link
+          aria-label="Reset password"
+          href="/signin/reset-password"
+          className="text-sm text-primary underline-offset-4 transition-colors hover:underline"
+        >
+          Reset password
+        </Link>
+      </CardFooter>{' '}
+    </Card>
+  );
+}
