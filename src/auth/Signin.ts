4f14fbafdccf2b870423d7cdb22cc53577691ba2
Hayat ilyas <ihayat855@gmail.com>
2023-07-11 11:48:17 +0530

    signin api and asynchandler

---
+++ src/auth/Signin.ts
@@ -0,0 +1,48 @@
+import { Response, Request, NextFunction } from 'express';
+import { httpStatusCode } from '../types/httpStatusCode';
+import prisma from '../config/databaseConfig';
+import bcrypt from 'bcrypt';
+import { getAccessToken, getRefreshToken } from '../utils/jwt';
+import { payload } from '../types';
+import logger from '../utils/logger';
+import asyncHandler from '../utils/asyncHandler';
+
+const Signin = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body;
+    logger.info(`${email} ${password}`);
+    const isUserExist = await prisma.user.findUnique({
+      where: {
+        email: email as string,
+      },
+    });
+    logger.info(isUserExist);
+    if (!isUserExist) {
+      return res
+        .status(httpStatusCode.BAD_REQUEST)
+        .json({ error: `${email} does not exist.` });
+    }
+
+    const isValidPassword = await bcrypt.compare(
+      password as string,
+      isUserExist.password,
+    );
+    logger.info(isValidPassword);
+    if (!isValidPassword)
+      return res
+        .status(httpStatusCode.BAD_REQUEST)
+        .json({ error: 'Invalid email/password' });
+    const payload: payload = {
+      id: isUserExist.id,
+      name: isUserExist.name,
+      email: isUserExist.email,
+      roleId: isUserExist.roleId,
+      created_at: isUserExist.created_at,
+    };
+    const AccessToken = await getAccessToken(payload);
+    const RefreshToken = await getRefreshToken(payload);
+    return res.status(httpStatusCode.OK).json({ AccessToken, RefreshToken });
+  },
+);
+
+export default Signin;
