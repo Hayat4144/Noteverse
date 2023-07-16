75c47dc4fcf64c3719c4f75ca5b32cda7edee0c1
Hayat ilyas <ihayat855@gmail.com>
2023-07-16 12:00:07 +0530

    adding type safety for the task

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,25 @@ export interface payload extends JwtPayload {
   id: string;
   created_at: Date;
 }
+
+export enum TaskPriority {
+  High = 'High',
+  Low = 'Low',
+  Medium = 'Medium',
+}
+
+export enum Status {
+  'Not Started',
+  'In Progress',
+  'Completed'
+}
+
+export interface createTaskInput{
+  title:string,
+  description? :string,
+  status:string,
+  priority:TaskPriority
+  due_date:Date,
+  is_standalone: boolean
+  assignee :string,
+}
