58e7790fcf9f02a6391c113af5a7bdd85c8ecf24
Hayat ilyas <ihayat855@gmail.com>
2023-07-18 21:54:54 +0530

    adding search api.

--- src/Tasks/api/searchApi.ts
+++ src/Tasks/api/searchApi.ts
@@ -1,20 +1,36 @@
+import { SeachFrilters, SearchSort } from '@/types';
 import { httpStatusCode } from "@/types/httpStatusCode";
 import taskObject from "@/utils/TaskObject";
 import asyncHandler from "@/utils/asyncHandler";
 import { Response,Request,NextFunction } from "express";
 import {URL} from  'url'
-import querystring from 'querystring'
-import logger from "@/utils/logger";
+import filterSort from "@/utils/filterSort";
+import pagination from '@/utils/pagination';
+import logger from '@/utils/logger';
+import { filtersQuery, sort } from '@/utils/SearchQuery';
+
+
 
 const searchApi = asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
-const {search} = new URL(req.url,`http://${req.headers.host}`)
-   console.log('search')
-   const m  = querystring.unescape(search)
-   console.log(m)
-   const escaprestring = querystring.escape('filter=dueDate:lt:2020-05-11T07::00::00.000Z&filter=price:lte:100&sort_by=desc(last_modified),asc(email)')
-//    console.log('escapestring',escaprestring)
-  return res.status(200).send('ok')
-    // return res.status(200).json({query,data:typeof query})
+  const {search} = new URL(req.url,`http://${req.headers.host}`)
+  const {filters,Sorts} = filterSort(search)
+  let {page}:any = req.query;
+  page = Number(page)
+  const sortIn = sort(Sorts)
+  const skip = pagination(2,page)
+  if(filters.length < 1){ 
+    const tasks = await taskObject.getTask(req.user_id,sortIn,skip);
+    if(!tasks) return res.status(httpStatusCode.OK).json({error:"you have not added tasks."})
+    return res.status(httpStatusCode.OK).json({data:tasks})
+  }
+  const filterQueries = filtersQuery(filters)
+  const tasks = await taskObject.searchApifilters(filterQueries)   
+  return res.status(httpStatusCode.OK).json({data:tasks})
+
 })
+
+
+
+
 
 export default searchApi;
