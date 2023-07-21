f149f8932042a0ec536e3e5526028755e14514a4
Hayat ilyas <ihayat855@gmail.com>
2023-07-21 17:34:14 +0530

    adding interfaces related to task .

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,10 +1,44 @@
+export interface JwtPayload {
+  id: string;
+  name: string;
+  email: string;
+  roleId: string;
+  created_at: Date;
+  iat: number;
+  exp: number;
+}
 
-interface JwtPayload {
-    id:string,
-    name:string,
-    email:string,
-    roleId:string,
-    created_at:Date,
-    iat:number,
-    exp:number
+export enum TaskPriority {
+  High = 'High',
+  Low = 'Low',
+  Medium = 'Medium',
+}
+
+export enum TaskStatus {
+  Not_Started ='Not Started',
+  In_Progress ='In Progress',
+  Completed = 'Completed',
+}
+
+export interface taskObject {
+  id: string;
+  title: string;
+  description?: string | null;
+  due_date: string;
+  status: string;
+  priority: TaskPriority;
+  parent_id: string | null;
+  is_standalone: boolean;
+  assignee: string;
+  userId: string;
+  createdAt: string;
+  updatedAt: string;
+  tags: [];
+}
+
+export interface TaskResponse {
+  data: taskObject[];
+  resultPerPage: number;
+  totalResults: number;
+  error?: string | undefined;
 }
