a2cef392ff52e8661474e61fa1af68091f19c359
Hayat ilyas <ihayat855@gmail.com>
2023-08-01 10:39:09 +0530

    sorty by bug fixed.

--- src/Tasks/Task.ts
+++ src/Tasks/Task.ts
@@ -1,66 +1,85 @@
-import prisma from "@/config/databaseConfig";
-import { createTaskInput } from "@/types";
-import { Task as PrismaTask } from "@prisma/client";
+import prisma from '@/config/databaseConfig';
+import { createTaskInput } from '@/types';
+import { Task as PrismaTask } from '@prisma/client';
 
-class Task{
-    async createTask(taskData:createTaskInput,userId:string){
-        const newtask = await prisma.task.create({
-            data:{...taskData,user:{connect:{id:userId}}} 
-        }) 
-        if(newtask){
-            return newtask;
-        }
+class Task {
+  async createTask(taskData: createTaskInput, userId: string) {
+    const newtask = await prisma.task.create({
+      data: { ...taskData, user: { connect: { id: userId } } },
+    });
+    if (newtask) {
+      return newtask;
     }
-    async getTaskById(id:string,userId:string):Promise<PrismaTask | null>{
-        const isTaskExist = await this.isTaskExist(id,userId);
-        return isTaskExist ;
-    }
-    async findByIdAndUpdate(id:string,taskData:createTaskInput,userId:string):Promise<PrismaTask | null>{
-        const isTaskExist = await this.isTaskExist(id,userId);
-        if(!isTaskExist) return null;
-        const updatedTask = await prisma.task.update({
-            where:{id:isTaskExist.id},
-            data:{...taskData}
-        })
-        return updatedTask ;
-    }
-    private async isTaskExist(id:string,userId:string):Promise<PrismaTask | null>{
-        const taskExist = await prisma.task.findUnique({
-            where:{id,userId}
-        })
-        if(!taskExist) return null;
-        return taskExist;
-    }
-    async findByIdAndDelete(id:string,userId:string):Promise<{title:string} | null>{
-        const isTaskExist = await this.isTaskExist(id,userId);
-        if(!isTaskExist) return null;
-        const deletedTask = await prisma.task.delete({
-            where:{id},
-            select:{
-                title:true
-            }
-        })
-        return deletedTask ;
-    }
-    async getTask(userId:string,sortIn:object,skip:number){
-        const tasks = await prisma.task.findMany({
-            take:20,
-            skip,
-            where:{userId},
-            orderBy:sortIn
-        })
-        return tasks
-    }
-    async searchApifilters(filterQuery:object,sortQuery:object,skip:number){
-        console.log(filterQuery)
-        const tasks = await prisma.task.findMany({
-            take:20,
-            skip,
-            where:filterQuery,
-            orderBy:sortQuery
-        })
-        return tasks
-    }
+  }
+
+  async getTaskById(id: string, userId: string): Promise<PrismaTask | null> {
+    const isTaskExist = await this.isTaskExist(id, userId);
+    return isTaskExist;
+  }
+
+  async findByIdAndUpdate(
+    id: string,
+    taskData: createTaskInput,
+    userId: string,
+  ): Promise<PrismaTask | null> {
+    const isTaskExist = await this.isTaskExist(id, userId);
+    if (!isTaskExist) return null;
+    const updatedTask = await prisma.task.update({
+      where: { id: isTaskExist.id },
+      data: { ...taskData },
+    });
+    return updatedTask;
+  }
+
+  private async isTaskExist(
+    id: string,
+    userId: string,
+  ): Promise<PrismaTask | null> {
+    const taskExist = await prisma.task.findUnique({
+      where: { id, userId },
+    });
+    if (!taskExist) return null;
+    return taskExist;
+  }
+
+  async findByIdAndDelete(
+    id: string,
+    userId: string,
+  ): Promise<{ title: string } | null> {
+    const isTaskExist = await this.isTaskExist(id, userId);
+    if (!isTaskExist) return null;
+    const deletedTask = await prisma.task.delete({
+      where: { id },
+      select: {
+        title: true,
+      },
+    });
+    return deletedTask;
+  }
+
+  async getTask(userId: string, sortIn: object | [], skip: number) {
+    const tasks = await prisma.task.findMany({
+      take: 20,
+      skip,
+      where: { userId },
+      orderBy: sortIn,
+    });
+    return tasks;
+  }
+
+  async searchApifilters(
+    filterQuery: object,
+    sortQuery: object | [],
+    skip: number,
+  ) {
+    const tasks = await prisma.task.findMany({
+      take: 20,
+      skip,
+      where: filterQuery,
+      orderBy: sortQuery,
+    });
+    return tasks;
+  }
 }
 
 export default Task;
--- src/utils/SearchQuery.ts
+++ src/utils/SearchQuery.ts
@@ -1,72 +1,83 @@
-import { IncaseSensitiveOperators, PrismaOperators, SeachFrilters, SearchSort } from "@/types";
+import {
+  IncaseSensitiveOperators,
+  PrismaOperators,
+  SeachFrilters,
+  SearchSort,
+} from '@/types';
 
-const filtersQuery = (filters:SeachFrilters[])=>{
-    let filterQuery: any = {};
-    const numberRegex = new RegExp('^[0-9]+$'); 
-    const dateRegex =  /^\d{4}-\d{2}-\d{2}$/ ;
-    const arrayRegex = /^\[.*\]$/ ; 
-    const IncaseSensitive ={mode:'insensitive'};
+const filtersQuery = (filters: SeachFrilters[]) => {
+  let filterQuery: any = {};
+  const numberRegex = new RegExp('^[0-9]+$');
+  const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+  const arrayRegex = /^\[.*\]$/;
+  const IncaseSensitive = { mode: 'insensitive' };
 
+  const incaseSensitiveOpertaors: IncaseSensitiveOperators[] = [
+    PrismaOperators.contains,
+    PrismaOperators.endsWith,
+    PrismaOperators.startsWith,
+  ];
 
-    const incaseSensitiveOpertaors:IncaseSensitiveOperators[] = [
-      PrismaOperators.contains,
-      PrismaOperators.endsWith,
-      PrismaOperators.startsWith,
-    ];
-
-    filters.forEach((filter) => {
-      // check if the filter value is conatains array
-      if(arrayRegex.test(filter.value)){ 
-        filter.value = filter.value.trim();
-        filter.value = JSON.parse(filter.value)
-      }
-      const filterOperatorValue:any= { [filter.opt]: filter.value };
-  
-     // Check if the filter value is a number and convert it to a number if needed
-      if(numberRegex.test(filter.value)){
-        if(!isNaN(Number(filter.value))){
-          filterOperatorValue[filter.opt] = Number(filter.value) 
-        }
-      }
-      
-      // check is filter value is date
-      if(dateRegex.test(filter.value)){
-        const dateObject = new Date(filter.value).toISOString();
-        filterOperatorValue[filter.opt] = dateObject;
-      }
-
-     
-      if (filterQuery[filter.field]) {
-        // Merge the existing filter with the new filterOperatorValue
-        filterQuery[filter.field] = {
-          ...filterQuery[filter.field],
-          ...filterOperatorValue,
-          // ...IncaseSensitive
-        };
-      } else {
-        // Create a new entry for the field in filterQuery
-        filterQuery[filter.field] = {...filterOperatorValue};
-      }
-      if(incaseSensitiveOpertaors.includes(filter.opt as IncaseSensitiveOperators)){
-        filterQuery[filter.field] = {...filterQuery[filter.field],...IncaseSensitive}
-      }
-    });
-    return filterQuery;
-}
-
-
-const sort  = (Sorts:SearchSort [])=>{
-    // default sorting by id in ascending order
-    let  sortIn:any = {};
-    if(Sorts.length > 0){
-      Sorts.map((item)=>{
-        sortIn[item.field]  = item.value;
-      })    
+  filters.forEach((filter) => {
+    // check if the filter value is conatains array
+    if (arrayRegex.test(filter.value)) {
+      filter.value = filter.value.trim();
+      filter.value = JSON.parse(filter.value);
     }
-    else{
-      sortIn['id'] = 'asc'
-    }  
+    const filterOperatorValue: any = { [filter.opt]: filter.value };
+
+    // Check if the filter value is a number and convert it to a number if needed
+    if (numberRegex.test(filter.value)) {
+      if (!isNaN(Number(filter.value))) {
+        filterOperatorValue[filter.opt] = Number(filter.value);
+      }
+    }
+
+    // check is filter value is date
+    if (dateRegex.test(filter.value)) {
+      const dateObject = new Date(filter.value).toISOString();
+      filterOperatorValue[filter.opt] = dateObject;
+    }
+
+    if (filterQuery[filter.field]) {
+      // Merge the existing filter with the new filterOperatorValue
+      filterQuery[filter.field] = {
+        ...filterQuery[filter.field],
+        ...filterOperatorValue,
+        // ...IncaseSensitive
+      };
+    } else {
+      // Create a new entry for the field in filterQuery
+      filterQuery[filter.field] = { ...filterOperatorValue };
+    }
+    if (
+      incaseSensitiveOpertaors.includes(filter.opt as IncaseSensitiveOperators)
+    ) {
+      filterQuery[filter.field] = {
+        ...filterQuery[filter.field],
+        ...IncaseSensitive,
+      };
+    }
+  });
+  return filterQuery;
+};
+
+const sort = (Sorts: SearchSort[]) => {
+  // default sorting by title in ascending order
+  let sortIn: any = {};
+  if (Sorts.length > 1) {
+    return Sorts.map((item) => {
+      return { [item.field]: item.value };
+    });
+  } else if (Sorts.length === 1) {
+    Sorts.map((item) => {
+      sortIn[item.field] = item.value;
+    });
+    return sortIn;
+  } else {
+    sortIn['title'] = 'asc';
     return sortIn;
   }
+};
 
-export {filtersQuery,sort};
+export { filtersQuery, sort };
