34bd678d1091ec7a9f011bdbab44817737765aba
Hayat ilyas <ihayat855@gmail.com>
2023-09-11 16:10:52 +0530

    adding recent and old notebook in the home page.

--- client/src/components/Home/ScratchPad.tsx
+++ client/src/components/Home/ScratchPad.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import ScratchPadEditor from './ScratchPadEditor';
+import { ScrollArea } from '../ui/scroll-area';
 
 export default function ScratchPad() {
   return (
-    <Card className="h-80 col-span-2">
-      <CardHeader className="py-3 capitalize">
-        <CardTitle>Scratchpad</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <ScratchPadEditor />
-      </CardContent>
+    <Card className="col-span-2">
+      <ScrollArea className="h-80">
+        <CardHeader className="py-3 capitalize">
+          <CardTitle>Scratchpad</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ScratchPadEditor />
+        </CardContent>
+      </ScrollArea>
     </Card>
   );
 }
