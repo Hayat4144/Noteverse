11cc268f82f5bde4beb4354798601a8c31ea31b1
Hayat ilyas <ihayat855@gmail.com>
2023-09-12 08:59:18 +0530

    Adding backend url configuration.

--- client/src/lib/BASE_URL.ts
+++ client/src/lib/BASE_URL.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'http:90000' ;
+export const BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8000'
+    : process.env.BACKEND_URL;
